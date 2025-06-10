328103fbde3b13abeec2d80493c5b31f8136001c
Param Chandarana <parambchandarana@gmail.com>
2025-06-10 16:44:08 +0530

    chore: setup api client with axios

---
+++ frontend/src/api/apiClient.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "/api", // Base URL for all API requests
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
